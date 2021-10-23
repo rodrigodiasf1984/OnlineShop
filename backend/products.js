59d2540c9c811d93e4e55b4575d0636659759a38
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-23 15:15:36 +0200

    refactor: added external url for the images

--- backend/products.js
+++ backend/products.js
@@ -4,21 +4,24 @@ const products = [
     name: 'Iphone 13 pro max',
     desc: '6.7-inch display',
     price: 999,
-    image: '',
+    image:
+      'https://res.cloudinary.com/da7uqniqz/image/upload/v1634994548/online-shop/iphone-13-pro_vdru3v.png',
   },
   {
     id: 2,
     name: 'Iphone 13',
     desc: '6.1-inch display',
     price: 899,
-    image: '',
+    image:
+      'https://res.cloudinary.com/da7uqniqz/image/upload/v1634994548/online-shop/iphone-13_vuybtk.png',
   },
   {
     id: 3,
     name: 'Iphone 13 mini',
     desc: '5.4-inch display',
     price: 799,
-    image: '',
+    image:
+      'https://res.cloudinary.com/da7uqniqz/image/upload/v1634994548/online-shop/iphone-13-mini_uklmiq.png',
   },
 ];
 
