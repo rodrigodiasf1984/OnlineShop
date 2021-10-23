a5d4ee4c4ee7e75b898d5444809fd350f2ff57ae
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-23 14:02:28 +0200

    feat: added nodemon , express, cors and some requests

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: 'Iphone 13 pro max',
+    desc: '6.7-inch display',
+    price: 999,
+    image: '',
+  },
+  {
+    id: 2,
+    name: 'Iphone 13',
+    desc: '6.1-inch display',
+    price: 899,
+    image: '',
+  },
+  {
+    id: 3,
+    name: 'Iphone 13 mini',
+    desc: '5.4-inch display',
+    price: 799,
+    image: '',
+  },
+];
+
+module.exports = products;
