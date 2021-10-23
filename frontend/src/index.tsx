039ddb411f0fd87641fd402dd3529b491b961636
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-23 15:31:04 +0200

    feat: added store to manager global states

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,20 @@
 import ReactDOM from 'react-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { productsReducer } from './slices';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore({
+  reducer: { products: productsReducer },
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
