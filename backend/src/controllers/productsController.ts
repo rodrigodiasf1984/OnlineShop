de80af45a7720ef4cd27f57073615c4464e8155e
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-24 19:23:01 +0200

    feat: updated controller

--- backend/src/controllers/productsController.ts
+++ backend/src/controllers/productsController.ts
@@ -1,20 +1,44 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
-import Products from '../models/Products';
+import Product from '../models/Product';
+import productsView from '../view/products_view';
 
 export default {
-  async create(req: Request, res: Response) {
-    const { name, description, price, image } = req.body;
-    const productsRepository = getRepository(Products);
+  async index(_req: Request, res: Response) {
+    const productsRepository = getRepository(Product);
+    const products = await productsRepository.find({
+      order: { name: 'ASC' },
+      relations: ['image'],
+    });
+    return res.json(productsView.renderMany(products));
+  },
 
-    const product = productsRepository.create({
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+    const productsRepository = getRepository(Product);
+    const product = await productsRepository.findOneOrFail(id, {
+      relations: ['image'],
+    });
+    return res.json(productsView.render(product));
+  },
+
+  async create(req: Request, res: Response) {
+    const { name, description, price } = req.body;
+    const productRepository = getRepository(Product);
+    const requestImage = req.file;
+    const image = { path: requestImage?.filename };
+
+    const data = {
       name,
       description,
       price,
       image,
-    });
+    };
 
-    await productsRepository.save(product);
+    const product = productRepository.create(data);
+
+    await productRepository.save(product);
+    // eslint-disable-next-line no-console
     return res.status(201).json(product);
   },
 };
