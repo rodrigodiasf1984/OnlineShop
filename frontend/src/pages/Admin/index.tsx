e13989eb19941a0561769f245db7a3e314007c16
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2021-10-25 00:28:21 +0200

    feat: admin page

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
-import { useForm, SubmitHandler } from 'react-hook-form';
-// import { Container } from './styles';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-console */
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Container } from './styles';
 
-const Admin = () => <h1>Admin</h1>;
+interface IFormInputs {
+  name: string;
+  description: string;
+  price: number;
+  image: File;
+}
+
+const schema = Yup.object({
+  name: Yup.string().required(),
+  description: Yup.string().required(),
+  price: Yup.number().positive().integer().required(),
+  image: Yup.mixed().required(),
+});
+
+const Admin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data: IFormInputs) => {
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <h1>Admin</h1>
+      <section className="body">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>Product Name</label>
+            <input {...register('name')} />
+            {errors.name && <p>{errors.name.message}</p>}
+          </div>
+          <div style={{ marginBottom: 10 }}>
+            <label>Product description</label>
+            <input {...register('description')} />
+            {errors.description && <p>{errors.description.message}</p>}
+          </div>
+          <div>
+            <label>Product price</label>
+            <input
+              type="number"
+              {...register('price', { valueAsNumber: true })}
+            />
+            {errors.price && <p>{errors.price.message}</p>}
+          </div>
+          <div>
+            <label>Image</label>
+            <input type="file" {...register('image')} />
+            {errors.image && <p>{errors.image.message}</p>}
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      </section>
+    </Container>
+  );
+};
 
 export default Admin;
--- frontend/src/pages/Admin/styles.ts
+++ frontend/src/pages/Admin/styles.ts
@@ -1,3 +1,93 @@
 import styled from 'styled-components';
+import { converterToRem } from '../../utils/converterToRem';
 
-export const Container = styled.div``;
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: ${converterToRem(100)}rem;
+
+  @media (min-width: 768px) {
+    padding-top: ${converterToRem(50)}rem;
+  }
+  @media (min-width: 1024px) {
+    padding-top: ${converterToRem(80)}rem;
+  }
+
+  @media (min-width: 1280px) {
+    padding-top: 0;
+    margin-bottom: ${converterToRem(100)}rem;
+  }
+
+  .body {
+    background: var(--gray-600);
+    width: 95%;
+    padding: ${converterToRem(20)}rem;
+    padding-bottom: ${converterToRem(20)}rem;
+    align-self: center;
+    border-radius: ${converterToRem(16)}rem;
+    max-width: ${converterToRem(600)}rem;
+  }
+
+  form {
+    max-width: ${converterToRem(500)}rem;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-weight: 100;
+    color: var(--gray-600);
+    text-align: center;
+    padding-bottom: ${converterToRem(10)}rem;
+  }
+
+  p {
+    color: var(--shape);
+  }
+
+  p::before {
+    display: inline;
+    content: '⚠ ';
+  }
+
+  input {
+    display: block;
+    box-sizing: border-box;
+    width: 100%;
+    border-radius: 4px;
+    border: 1px solid white;
+    padding: ${converterToRem(10)}rem ${converterToRem(15)}rem;
+    font-size: ${converterToRem(16)}rem;
+  }
+
+  label {
+    text-align: left;
+    display: block;
+    margin-bottom: 4px;
+    margin-top: ${converterToRem(10)}rem;
+    color: white;
+    font-size: ${converterToRem(16)}rem;
+    font-weight: 200;
+  }
+
+  button {
+    display: block;
+    width: 100%;
+    background: var(--red);
+    border: none;
+    margin-bottom: ${converterToRem(20)}rem;
+    margin-top: ${converterToRem(30)}rem;
+    text-transform: uppercase;
+    padding: ${converterToRem(20)}rem;
+    border-radius: 4px;
+    color: var(--shape);
+    padding: ${converterToRem(20)}rem;
+    font-size: ${converterToRem(16)}rem;
+    font-weight: 100;
+    letter-spacing: ${converterToRem(10)}rem;
+    transition: background 0.2s;
+  }
+
+  button:hover {
+    opacity: 0.8;
+  }
+`;
